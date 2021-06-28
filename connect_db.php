d52d6b82fe12713cf8994b3df57283176f08d3cc
Theerawat61 <forworkonly42@gmail.com>
2021-06-28 15:32:32 +0700

    connect dp

---
+++ connect_db.php
@@ -0,0 +1,50 @@
+<!doctype html>
+
+<html lang="th">
+
+<head>
+	<title>Computer-Based Learning</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	
+	<div class="wrapper d-flex align-items-stretch">
+		<!-- include sidebar -->
+		<?php include ('sidebar_user.php'); ?>
+
+		<!-- Page Content  -->
+		<div id="content" class="p-4 p-md-5 pt-5">
+			<h2 class="mb-4">การเชื่อมต่อฐานข้อมูล</h2>
+            <div style="text-align:center;">
+			    <p><iframe width="560" height="315" src="https://www.youtube.com/embed/pca8LKeYpT0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
+            </div>
+
+            <h4>การเชื่อมต่อ database</h4>
+            <img src="/cbl/images/connect_db.PNG" width="800"><br><br><br>
+
+            <h6 style="text-indent: 2.5em;">หลังจากที่ได้ทำการเชื่อมต่อฐานแล้ว มาดูตัวอย่าง การ select , insert , update , delete  ตาราง product  ซึ่งตารางเก็บข้อมูลสินค้า ด้างล่างกันเลย</h6><br>
+
+            <h4>การ Select</h4>
+            <img src="/cbl/images/select.PNG" width="800"><br><br><br>
+
+            <h4>การ Update</h4>
+            <img src="/cbl/images/update.PNG" width="800"><br><br><br>
+
+            <h4>การ Delete</h4>
+            <img src="/cbl/images/del.PNG" width="800"><br><br><br>
+            
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+</html>
