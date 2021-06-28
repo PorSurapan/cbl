b9f59bcd3f29fdd93a324bd4c21c34c3d41dd6af
ninesam12 <naynine789@gmail.com>
2021-06-28 16:19:47 +0700

    quiz

--- quiz.js
+++ quiz.js
@@ -2,20 +2,20 @@ function check(){
 
     var question1 = document.quiz.question1.value;
     var question2 = document.quiz.question2.value;
-    var question3 = document.quiz.question3.value;
-    var question4 = document.quiz.question4.value;
-    var question5 = document.quiz.question5.value;
-    var question6 = document.quiz.question6.value;
-    var question7 = document.quiz.question7.value;
-    var question8 = document.quiz.question8.value;
-    var question9 = document.quiz.question9.value;
-    var question10 = document.quiz.question10.value;
     var correct = 0;
 
-    if (question1 == "CREATE TABLE"){
+    if (question1 == "CREATE DATABASE testDB"){
+        document.getElementById("textbox1").style.color = "green";
         correct++;
-    }
+    }if (question2 == "CREATE TABLE"){
+        document.getElementById("textbox2").style.color = "green";
+        correct++;
+        }if(question1 !== "CREATE DATABASE testDB" && question2 !== "CREATE TABLE"){
+            document.getElementById("textbox1").style.color = "red";
+            document.getElementById("textbox2").style.color = "red";
+        }
+    
 
     document.getElementById("after_submit").style.visibility = "visible";
-    document.getElementById("number_correct").innerHTML = "คุณได้ " + correct + " คะแนน";
+    document.getElementById("number_correct").innerHTML = "คุณได้ " + correct + " คะแนนจากทั้งหมด 10 ข้อ";
 }
