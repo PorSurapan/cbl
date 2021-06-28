7400fa4afd708014042d1fc468b3b2b989247708
PorSurapan <por0839360604@gmail.com>
2021-06-28 14:36:09 +0700

    search complete

---
+++ searchUser.js
@@ -0,0 +1,35 @@
+var xmlHttp;
+
+function createXMLHttpRequest() {
+    if (window.ActiveXObject)
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    else
+        xmlHttp = new XMLHttpRequest();
+}
+
+function stateChange() {
+    if (xmlHttp.readyState == 4 || xmlHttp.readyState == "complete") {
+        var allText = xmlHttp.responseText;
+        
+        //alert(allText);
+        var temp = new Array();
+        temp = allText.split("^");
+
+        var options = '';
+        for (var i = 0; i < temp.length; i++) {
+            options += '<option value="' + temp[i] + '" />';
+        }
+
+        document.getElementById('word').innerHTML = options;
+    }
+}
+
+function searchUser(str)
+{
+    createXMLHttpRequest();
+    xmlHttp.onreadystatechange = stateChange;
+    var url = "userSearch.php";
+    url = url + "?word=" + str;
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
