8ad0de57043386cc3416e72c5e477e0632c18051
ninesam12 <naynine789@gmail.com>
2021-06-28 14:36:38 +0700

    upser tools

---
+++ tools.php
@@ -0,0 +1,120 @@
+<!doctype html>
+
+<html lang="th">
+
+<head>
+	<title>Computer-Based Learning</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	
+	<div class="wrapper d-flex align-items-stretch">
+		
+    <?php include ('sidebar_user.php'); ?>
+
+		<!-- Page Content  -->
+		<div id="content" class="p-4 p-md-5 pt-5">
+			<h2 class="mb-4">เครื่องมือที่ใช้ในการเรียน</h2>
+            <h4>Google Chrome</h4>
+            <h6 style="text-indent: 2.5em;">เป็นซอฟต์แวร์เว็บเบราว์เซอร์ พัฒนาโดย Google เปิดตัวครั้งแรกในปี 2008 โดยยังรองรับระบบปฏิบัติการ Windows ก่อนจะออกโปรแกรมให้กับ Linux, macOS, IOS และ Android ในภายหลัง Google Chrome ยังเป็นองค์ประกอบหลักของ ChromeOS ซึ่งจะทำหน้าที่เป็นหน้าจอหลักสำหรับการเรียกใช้เว็บแอป</h6>
+            <h5 style="text-indent: 1.5em;">วิธีการติดตั้ง Google Chrome</h5>
+            <h6 style="text-indent: 3em;">1. คลิกเปิดเบราว์เซอร์ Internet explorer จากนั้นเข้าไปที่เว็บไซต์ <a href="https://www.google.com/intl/th/chrome/" target="_blank">https://www.google.com/intl/th/chrome/</a> และ Download Google Chorome<br><br></h6>
+            <img src="/cbl/images/chrome1.jpg" width="600"><br><br><br>
+            <h6 style="text-indent: 3em;">2. เมื่อเข้ามาแล้ว คลิกปุ่ม "ดาวน์โหลด Chrome"<br><br></h6>
+            <img src="/cbl/images/chrome2.jpg" width="600"><br><br><br>
+            <h6 style="text-indent: 3em;">3. ต่อด้วย คลิกปุ่ม "ยอมรับและติดตั้ง"<br><br></h6>
+            <img src="/cbl/images/chrome3.jpg" width="600"><br><br><br>
+            <h6 style="text-indent: 3em;">4. เข้าไปยังโฟลเดอร์ที่ดาวโหลดโปรแกรมเก็บไว้ จากนั้นคลิกเพื่อติดตั้งโปรแกรม Google Chrome<br><br></h6>
+            <img src="/cbl/images/chrome4.jpg" width="600"><br><br><br>
+            <h6 style="text-indent: 3em;">5. เมื่อติดตั้งเสร็จ จะมีการตั้งค่าตัวโปรแกรม โดยเริ่มจากกดปุ่ม "เริ่มต้นใช้งาน"<br><br></h6>
+            <img src="/cbl/images/chrome5.jpg" width="600"><br><br><br>
+
+			<h4>Visual Studio Code</h4>
+            <h6 style="text-indent: 3em;">เป็นโปรแกรมในการเขียนหรือแก้ไข source code ที่ถูกพัฒนาโดยบริษัท Microsoft สำหรับ Windows, Linux, macOS</h6>
+            <h5 style="text-indent: 1.5em;">วิธีการติดตั้ง Visual Studio Code</h5>
+            <h6 style="text-indent: 3em;">1. เข้าไปที่เว็บไซต์ <a href="https://code.visualstudio.com/download" target="_blank">https://code.visualstudio.com/download</a> และ Download โปรแกรม VS Code โดยเลือกให้ตรงกับ OS ของเครื่องคอมพิวเตอร์<br><br></h6>
+            <img src="/cbl/images/vscode1.jpg" width="800"><br><br><br>
+            <h6 style="text-indent: 3em;">2. ดับเบิ้ลคลิก หรือคลิกขวาและกด "Open" โปรแกรมที่ดาวน์โหลดมา<br><br></h6>
+            <img src="/cbl/images/vscode2.jpg" width="800"><br><br><br>
+            <h6 style="text-indent: 3em;">3. คลิกปุ่ม “Run”<br><br></h6>
+            <img src="/cbl/images/vscode3.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">4. เลือก "I accept the agreement" และคลิกปุ่ม "Next >"<br><br></h6>
+            <img src="/cbl/images/vscode4.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">5. เลือกพื้นที่ในการจัดเก็บโปรแกรม (แนะนำให้ใช้ Default ที่ให้มา) และคลิกปุ่ม "Next >"<br><br></h6>
+            <img src="/cbl/images/vscode5.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">6. คลิกปุ่ม “Next >”<br><br></h6>
+            <img src="/cbl/images/vscode6.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">7. เลือกส่วนเพิ่มงานให้เลือก Create a desktop icon และ Add to PATH (requires shell restart) จากนั้นให้คลิกปุ่ม "Next >"<br><br></h6>
+            <img src="/cbl/images/vscode7.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">8. คลิกปุ่ม "Install" เพื่อติดตั้งโปรแกรม<br><br></h6>
+            <table>
+                <tr>
+                    <td>
+                        <img src="/cbl/images/vscode8.jpg" width="400"><br><br><br>
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td>    
+                        <img src="/cbl/images/vscode8-2.jpg" width="400"><br><br><br>
+                    </td>
+                </tr>
+            </table>
+            <h6 style="text-indent: 3em;">9. คลิกปุ่ม "Finish" เสร็จสิ้นการติดตั้งโปรแกรม VS Code<br><br></h6>
+            <table>
+                <tr>
+                    <td>
+                        <img src="/cbl/images/vscode9.jpg" width="400"><br><br><br>
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td>    
+                        <img src="/cbl/images/vscode9-2.jpg" width="600"><br><br><br>
+                    </td>
+                </tr>
+            </table>
+
+            <h4>XAMPP</h4>
+            <h6 style="text-indent: 2.5em;">เป็นโปรแกรม Apache web server ไว้จำลอง web server เพื่อไว้ทดสอบ สคริปหรือเว็บไซต์ในเครื่องของเรา โดยที่ไม่ต้องเชื่อมต่ออินเตอร์เน็ตและไม่ต้องมีค่าใช้จ่ายใดๆ ง่ายต่อการติดตั้งและใช้งานโปรแกรม Xampp จะมาพร้อมกับ PHP ภาษาสำหรับพัฒนาเว็บแอพลิเคชั่นที่เป็นที่นิยม , MySQL ฐานข้อมูล, Apache จะทำหน้าที่เป็นเว็บ เซิร์ฟเวอร์</h6>
+            <h5 style="text-indent: 1.5em;">วิธีการติดตั้ง XAMMP</h5>
+            <h6 style="text-indent: 3em;">1. เข้าไปที่เว็บไซต์ <a href="https://www.apachefriends.org/index.html" target="_blank">https://www.apachefriends.org/index.html</a> และ Download โปรแกรม XAMPP โดยเลือกให้ตรงกับ OS ของเครื่องคอมพิวเตอร์<br><br></h6>
+            <img src="/cbl/images/xampp1.jpg" width="800"><br><br><br>
+            <h6 style="text-indent: 3em;">2. ดับเบิ้ลคลิก หรือคลิกขวาและกด "Open" โปรแกรมที่ดาวน์โหลดมา<br><br></h6>
+            <img src="/cbl/images/xampp1-2.png" width="800"><br><br><br>
+            <h6 style="text-indent: 3em;">3. ทำการติดตั้ง คลิก Next<br><br></h6>
+            <img src="/cbl/images/xampp2.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">4. ทำการเลือก Components ที่ต้องการ แต่สำหรับการเรียนใช้แค่ Apache / MySQL เท่านี้<br><br></h6>
+            <img src="/cbl/images/xampp3.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">5. เลือก Path ในการติดตั้ง XAMPP<br><br></h6>
+            <img src="/cbl/images/xampp4.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">6. คลิกปุ่ม "Next >"<br><br></h6>
+            <img src="/cbl/images/xampp5.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">7. คลิกปุ่ม "Next >"<br><br></h6>
+            <img src="/cbl/images/xampp6.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">8. รอทำการติดตั้ง<br><br></h6>
+            <img src="/cbl/images/xampp7.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">9. ทำการติดตั้งเรียบร้อย และทำการ Restart Computer 1 ครั้ง<br><br></h6>
+            <img src="/cbl/images/xampp8.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;"> 10. จากนั้นเข้า Control Panel ของ XAMPP และทำการคลิก Start Apache และ Mysql ตามลำดับ<br><br></h6>
+            <img src="/cbl/images/xampp9.jpg" width="400"><br><br><br>
+            <h6 style="text-indent: 3em;">จากนั้นลองเข้า PhpMyAdmin และ localhost ดู<br><br></h6>
+            <h6 style="text-indent: 3em;"><a href="http://localhost/phpmyadmin">http://localhost/phpmyadmin</a><br><br></h6>
+            <img src="/cbl/images/xampp10.jpg" width="800"><br><br><br>
+            <h6 style="text-indent: 3em;">สำหรับ Code ต่างๆ ของ Php ในการทำ WebSite ให้เอามาไว้ที่ C:\xampp\htdocs\<br><br></h6>
+            <img src="/cbl/images/xampp11.jpg" width="600"><br><br><br>
+        </div>
+	</div>
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+</html>
